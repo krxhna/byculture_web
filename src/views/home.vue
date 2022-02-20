1b0f5bd0d7a6f2e916b35f94b73f89fab58d31a7
krxhna <m.krishnakanth1999@gmail.com>
2022-02-21 07:07:31 +0800

    first

---
+++ src/views/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <div class="text-6xl font-mono text-blue-600 font-bold">By Culture</div>
+    <div class="p-4">Find comapnies that matcvh your culture</div>
+    <div class="r">
+      <input
+        class="p-3 w-2/3 h-16 shadow-lg focus:outline-none"
+        
+        id="username"
+        type="text"
+        placeholder="Email"
+      />
+      <button class="bg-blue-600 h-16 px-5 text-xl p-3 text-blue-50 shadow-xl">get invite</button>
+    </div>
+  </div>
+</template>
